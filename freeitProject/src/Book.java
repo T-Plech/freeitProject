2fb36f4939d24d37ab25673d0dda8f605357fd1b
T-Plech <tricsi_k@mail.ru>
2021-04-27 14:57:46 +0300

    created class Book with constructor(author, title, pages)

---
+++ freeitProject/src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+
+    String author;
+    String title;
+    int pages;
+
+    Book(String author, String title, int pages){
+        this.author = author;
+        this.title=title;
+        this.pages=pages;
+
+    }
+
+}
