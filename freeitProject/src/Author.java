b75382fd56ec56b9c188d37d2e658aec381654ed
T-Plech <tricsi_k@mail.ru>
2021-04-27 15:00:35 +0300

    created class Author with constructor(name, nationality)

---
+++ freeitProject/src/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+
+    String name;
+    String nationality;
+
+    Author(String name, String nationality){
+        this.name = name;
+        this.nationality = nationality;
+    }
+
+}
